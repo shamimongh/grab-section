6dfbfbfca899a675eb2ef4ee592191aeb9d8354b
Shamim Bin Nur <echoo.shamim@gmail.com>
2023-03-30 14:10:04 +0600

    init

---
+++ main/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+class Student(models.Model):
+    student_id = models.CharField(primary_key=True, unique=True, max_length=255)
+    student_name = models.CharField(max_length=255)
+    student_password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.student_name
+
+class Course(models.Model):
+    course_id = models.CharField(primary_key=True, unique=True, max_length=255)
+    course_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.course_name
+
+class CompletedCourse(models.Model):
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.student_id) + " | " + str(self.course_id)
+
+class IncompleteCourse(models.Model):
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.student_id) + " | " + str(self.course_id)
